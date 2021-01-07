ef76494c95ccd1c1ad491741c44578de65233c94
Xavier Genolhac <x.genolhac@gmail.com>
2021-01-07 01:57:16 +0100

    display images OK

--- component/starwars-widget.component.js
+++ component/starwars-widget.component.js
@@ -1,29 +1,39 @@
-let lengthTable;
 class StarWarsWidgetElement extends HTMLElement {
-
+    
     constructor() {
         super();
         this.attachShadow({mode: 'open'})
+        this.lengthTable;
     
     this.shadowRoot.innerHTML = `
-    <div>Characteres</div>
-    <h3>
+        <style>
+            img {
+                width: 20%
+            }
+        </style>
+        <div class="starwars-widget">
+        <p>
+        <div id="pictureCharactere"></div>
         <span id="charactere">Jean</span>
-    </h3>
-    `
+        </p>
+        </div>
+        `
     }
 
     fetchAPIData() {
         fetch(`https://miadil.github.io/starwars-api/api/all.json`)
             .then (res => res.json())
             .then (res => {
-                lengthTable = res.length;
-                console.log("ma longueur :: " + lengthTable)
+                //console.log(res)
+                this.lengthTable = res.length;
+                //console.log("ma longueur :: " + this.lengthTable)
                 this._charactere = [];
-                this._charactere.lenght = res.length;
+                this._pictureCharactere = [];
                 for (let i=0; i<res.length; i++) {
-                    //console.log(res[i].name)
+                    //console.log(res[i].image)
                     this._charactere[i]=res[i].name
+                    this._pictureCharactere[i]=res[i].image
+                    console.log(this._pictureCharactere[i])
                 }
                 this.render()
             })
@@ -34,9 +44,20 @@ class StarWarsWidgetElement extends HTMLElement {
     }
 
     render() {
-        for (let i=0; i<lengthTable; i++) {
-            this.shadowRoot.querySelector('h3 #charactere').innerHTML += this._charactere[i] + "<br />"
+        for (let i=0; i<this.lengthTable; i++) {
+            
+            var img = new Image(200, 200);
+            img.src = this._pictureCharactere[i];
+      
+            var src = this.shadowRoot.getElementById("pictureCharactere");
+            console.log(src)
+            src.appendChild(img);
+            this.shadowRoot.getElementById('charactere').innerHTML += this._charactere[i] + "<br />"
         }
+        /* for (let i=0; i<this.lengthTable; i++) {
+            this.shadowRoot.querySelector('img').src += this._pictureCharactere[i] 
+            this.shadowRoot.querySelector('p #charactere').innerHTML += this._charactere[i] + "<br />"
+        } */
 
     }
 
